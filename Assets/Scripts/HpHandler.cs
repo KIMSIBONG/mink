1f83db0ef9bb38a01cd30284aca1cf423360c74b
KIMSIBONG <153712936+KIMSIBONG@users.noreply.github.com>
2024-06-01 21:57:10 +0900

    넉백 1차

--- Assets/Scripts/HpHandler.cs
+++ Assets/Scripts/HpHandler.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,15 +10,39 @@ public class HpHandler : MonoBehaviour
     public float maxHp = 200f;
     private float currentHp;
 
+    //³Ë¹é
+    public float KnockbackTime = 0.2f;
+    private float currentKnockbackTime = 0;
+    public float pushForce = 100f;
+    private Rigidbody2D rb;
+    public GameObject KnockBackObject;
+
+    private void Update()
+    {
+        if (currentKnockbackTime > 0)
+        {
+            currentKnockbackTime -= Time.deltaTime;
+            KnockBack();
+        }
+    }
+
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         currentHp = maxHp;
         slider.value = currentHp / maxHp;
     }
     
-    public void SetHp(float damage)
+    public void SetHp(float damage) 
     {
         currentHp -= damage;
         slider.value = currentHp / maxHp;
+        currentKnockbackTime = KnockbackTime;
+    }
+    private void KnockBack()
+    {
+        Vector2 pushDirection = -KnockBackObject.transform.forward * pushForce;
+        rb.AddForce(pushDirection, ForceMode2D.Impulse);
+
     }
 }
--- Assets/Scripts/SpawnSkill.cs
+++ Assets/Scripts/SpawnSkill.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SpawnManager : MonoBehaviour
+public class SpawnSkill : MonoBehaviour
 {
     public GameObject prefabToSpawn;  
     public Transform spawnLocation; 
